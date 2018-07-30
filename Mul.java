0e915268988dae29e5221a6fc66a43f7f708baf4
lekha005 <41885177+lekha005@users.noreply.github.com>
2018-07-30 09:17:10 +0000

    Create Mul.java

---
+++ Mul.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Mul
+{
+    public static void main(String hj[])
+    {
+        Scanner in=new Scanner(System.in);
+         int n=in.nextInt();
+         for(int i=n;i<=n*5;i=i+n)
+         {
+            System.out.print(i+" ");
+         }
+    }
+}

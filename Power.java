cc5af0d989cd5751836f8ebd724a71f43fdc763b
lekha005 <41885177+lekha005@users.noreply.github.com>
2018-07-30 08:28:11 +0000

    Create Power.java

---
+++ Power.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Power
+{
+    public static void main(String hj[])
+    {
+        Scanner in=new Scanner(System.in);
+         int n=in.nextInt();
+         int k=in.nextInt();
+         double y=Math.pow(n,k);
+        System.out.print((int)y);
+    }
+}
